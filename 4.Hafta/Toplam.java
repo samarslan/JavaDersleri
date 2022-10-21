aba0534831fdefcc49f3764b3a43b547d3e69f5b
samarslan <86328409+samarslan@users.noreply.github.com>
2022-10-21 10:41:53 +0300

    Create Toplam.java

---
+++ 4.Hafta/Toplam.java
@@ -0,0 +1,24 @@
+package org.example;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i=0, toplam=0;
+        int[] dizi= new int[10];
+        Scanner oku=new Scanner(System.in);
+        for (i=0; i<10;i++){
+            System.out.println("Sayı girin: ");
+            dizi[i]=oku.nextInt();
+        }
+        for(i=0;i<10;i++){
+            if(dizi[i]!=2&&dizi[i]%2==0){
+                toplam=toplam+dizi[i];
+            }
+        }
+        System.out.println(toplam);
+    }
+}
