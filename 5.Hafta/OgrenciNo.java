5d8142e0d1a4531ffa58ea07bb3286383c908f16
samarslan <86328409+samarslan@users.noreply.github.com>
2022-10-28 10:43:09 +0300

    Create OgrenciNo.java

---
+++ 5.Hafta/OgrenciNo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        long sayi;
+        Scanner oku=new Scanner(System.in);
+        sayi=oku.nextLong();
+        int[] dizi=new int[4];
+    }
+
+    public static String burc(int gun, int ay){
+
+    }
+}
