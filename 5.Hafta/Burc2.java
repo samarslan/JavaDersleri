1e3fa6d7a9ab71c0c4279af00d369312243d9c5c
samarslan <86328409+samarslan@users.noreply.github.com>
2022-10-28 10:25:38 +0300

    Create Burc2.java

---
+++ 5.Hafta/Burc2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner oku=new Scanner(System.in);
+        int gun,ay;
+        System.out.println("Doğduğunuz günü girin;");
+        gun=oku.nextInt();
+        System.out.println("Doğduğunuz ayı girin;");
+        ay= oku.nextInt();
+        System.out.println(burc(gun, ay));
+    }
+
+    public static String burc(int gun, int ay){
+
+    }
+}
