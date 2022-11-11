4a4cf1afe7263b407d1cb18ebf8b2649c13eac70
samarslan <86328409+samarslan@users.noreply.github.com>
2022-11-11 09:02:07 +0300

    Update BenzinYol.java

--- 4.Hafta/BenzinYol.java
+++ 4.Hafta/BenzinYol.java
@@ -1,25 +1,24 @@
-package intro;
-
 import java.util.Scanner;
 
-public class main {
+public class Main {
 
-	public static void main(String[] args) {
-		
-		Scanner input = new Scanner(System.in);
-		int benzin, yol=0, tl=0;
-		System.out.println("Alınan benzini giriniz:\n");
-		benzin = input.nextInt();
-		do {
-			System.out.println("Yüklenen benzin ile kaç km gittiniz: ");
-			yol=input.nextInt();
-			if(yol>1000) {
-				System.out.println("Km maximum 999 olmalıdır.");
-			}
-		}
-		while(yol>1000); {
-				System.out.println("Harcanan benzın mıktarı "+tl/yol);
+    public static void main(String[] args) {
 
-		}
-		}
-	}
+        Scanner input = new Scanner(System.in);
+        int benzin, yol = 0;
+        System.out.println("Alınan benzini giriniz:\n");
+        benzin = input.nextInt();
+        do {
+            System.out.println("Yüklenen benzin ile kaç km gittiniz: ");
+            yol = input.nextInt();
+            if (yol > 1000) {
+                System.out.println("Km maximum 999 olmalıdır.");
+            }
+        }
+        while (yol > 1000);
+        {
+            System.out.println("Harcanan benzın mıktarı " + benzin / yol);
+
+        }
+    }
+}
