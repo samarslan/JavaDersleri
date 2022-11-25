e6604ab4750f44fd26d52039f2ee1c0dd5dcbc8e
samarslan <86328409+samarslan@users.noreply.github.com>
2022-11-25 10:25:39 +0300

    Create Kare.java

---
+++ 9.Hafta/Kare.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] dizi={1,2,3,4,5,6,7,8,9};
+
+        for (int i = 0; i < dizi.length; i++) {
+            System.out.println(dizi[i]*dizi[i]);
+        }
+    }
+}
